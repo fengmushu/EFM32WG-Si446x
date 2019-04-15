89af103cdf4b88468694cb8130547af536f25d74
ken <ken@tt-cool.com>
2019-04-15 23:04:10 +0800

    compile & run ok

---
+++ GNU ARM v7.2.1 - Debug/Drivers/bsp_stk.d
@@ -0,0 +1,205 @@
+Drivers/bsp_stk.o: ../Drivers/bsp_stk.c \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\em_device.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg990f256.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\cmsis\include\core_cm4.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\cmsis\include\cmsis_version.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\cmsis\include\cmsis_compiler.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\cmsis\include\cmsis_gcc.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\cmsis\include\mpu_armv7.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\system_efm32wg.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_dma_ch.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_dma.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_aes.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_usb_hc.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_usb_diep.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_usb_doep.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_usb.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_msc.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_emu.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_rmu.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_cmu.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_lesense_st.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_lesense_buf.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_lesense_ch.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_lesense.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_ebi.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_fpueh.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_usart.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_timer_cc.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_timer.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_acmp.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_leuart.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_rtc.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_letimer.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_pcnt.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_i2c.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_gpio_p.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_gpio.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_vcmp.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_prs_ch.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_prs.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_adc.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_dac.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_lcd.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_burtc_ret.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_burtc.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_wdog.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_etm.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_dma_descriptor.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_devinfo.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_romtable.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_calibrate.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_prs_signals.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_dmareq.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_dmactrl.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_uart.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_af_ports.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_af_pins.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_cmu.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_assert.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_bus.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_gpio.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_gpio.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\hardware\kit\common\bsp\bsp.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\hardware\kit\common\halconfig\bsphalconfig.h \
+ C:/Users/SinaR/SimplicityStudio/v4_workspace/EFM32WG-Si446x/hal-config/hal-config.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\halconfig\inc\hal-config\hal-config-types.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\hardware\kit\common\bsp\bsp_bcp.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_usart.h \
+ e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_ebi.h
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\em_device.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg990f256.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\cmsis\include\core_cm4.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\cmsis\include\cmsis_version.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\cmsis\include\cmsis_compiler.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\cmsis\include\cmsis_gcc.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\cmsis\include\mpu_armv7.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\system_efm32wg.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_dma_ch.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_dma.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_aes.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_usb_hc.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_usb_diep.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_usb_doep.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_usb.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_msc.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_emu.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_rmu.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_cmu.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_lesense_st.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_lesense_buf.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_lesense_ch.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_lesense.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_ebi.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_fpueh.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_usart.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_timer_cc.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_timer.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_acmp.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_leuart.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_rtc.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_letimer.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_pcnt.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_i2c.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_gpio_p.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_gpio.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_vcmp.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_prs_ch.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_prs.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_adc.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_dac.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_lcd.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_burtc_ret.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_burtc.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_wdog.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_etm.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_dma_descriptor.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_devinfo.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_romtable.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_calibrate.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_prs_signals.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_dmareq.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_dmactrl.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_uart.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_af_ports.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\device\siliconlabs\efm32wg\include\efm32wg_af_pins.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_cmu.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_assert.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_bus.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_gpio.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_gpio.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\hardware\kit\common\bsp\bsp.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\hardware\kit\common\halconfig\bsphalconfig.h:
+
+C:/Users/SinaR/SimplicityStudio/v4_workspace/EFM32WG-Si446x/hal-config/hal-config.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\halconfig\inc\hal-config\hal-config-types.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\hardware\kit\common\bsp\bsp_bcp.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_usart.h:
+
+e:\siliconlabs\simplicitystudio\v4\developer\sdks\gecko_sdk_suite\v2.5\platform\emlib\inc\em_ebi.h:
